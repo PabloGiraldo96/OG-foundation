fe2ba3313c6f485e91e5661823f9640366e7ee36
Juan Pablo Jaramillo Giraldo <juanpablogiraldo9602@gmail.com>
2025-01-29 02:55:44 -0500

    adding SEO settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,41 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
+//import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+//const geistSans = localFont({
+//  src: "./fonts/GeistVF.woff",
+//  variable: "--font-geist-sans",
+//  weight: "100 900",
+//});
+//const geistMono = localFont({
+//  src: "./fonts/GeistMonoVF.woff",
+//  variable: "--font-geist-mono",
+//  weight: "100 900",
+//});
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "OG Foundation",
   description: "OG Foundation 2025",
   icons: {
     icon: "/favicon.ico",
+  },
+  metadataBase: new URL("https://www.ogfoundation.com"),
+  alternates: {
+    canonical: "/",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
   },
 };
 
@@ -28,11 +46,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
